d3fa414a95d20377832482523ed8524a9c872dca
Wilman TELLO BORJA <utellwi@ile.mtl.qc.ca>
2018-01-08 13:07:55 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+  rootPage:any = TabsPage;
+  pages: Array<{title: string, component: any}>;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Autres apps', component: AboutPage },
+      { title: 'Partager sur facebook', component: AboutPage },
+    ];
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  openHome() {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(TabsPage);
+  }
+}
